38629c7343d05dd8e35b349da907e7fe80df879e
IvanRao <ivan.rao@hotmail.com>
2018-06-06 20:27:34 -0300

    Prueba react

--- Linea101-Node/src/client/index.jsx
+++ Linea101-Node/src/client/index.jsx
@@ -3,32 +3,102 @@ import {render} from 'react-dom';
 
 function MyButton (props){
     return (
-      <button onClick={props.onToggle}>
-        {props.toggle ? 'Prendido' : 'Apagado'}
+      <button onClick={props.onClick}>
+        {props.text}
       </button>
     );
 }
 
-class Toggle extends React.Component {
+function MyInput (props){
+  return (
+    <input type={props.type} id={props.id} name={props.name}>
+    </input>
+  )
+}
+
+class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {isToggleOn: true};
-    this.handleClick = this.handleClick.bind(this);
+    this.state = { tasks : [] , pageTitle : "Task List" , buttonName : "Crear" };
+    this.toggleTask = this.toggleTask.bind(this);
+    this.createTask = this.createTask.bind(this)
+    this.deleteTask = this.deleteTask.bind(this)
   }
 
-  handleClick() {
-    this.setState(prevState => ({
-      isToggleOn: !prevState.isToggleOn
-    }));
+  componentDidMount(){
+    this.updateList()
+  }
+
+  updateList(){
+    fetch("/tasks")
+      .then( res => res.json() )
+      .then( tasks => {
+        this.setState({tasks})
+      })
+  }
+
+  createTask(){
+    let newTask = {
+      title: document.getElementById("newTaskName").value
+    }
+    if (newTask.title.length > 0) {
+      fetch("/tasks", {method:"POST", headers:{'content-type': 'application/json'}, body : JSON.stringify(newTask)})
+        .then( (resp) => resp.json() )
+        .then (msg => {
+          this.updateList(),
+          console.log(msg) 
+        })
+    }else {
+      console.log("Input vacío")
+    }
+  }
+  
+  deleteTask(id){
+    fetch("/task/" + id + "/toggle", {method:"delete"})
+      .then( (resp) => resp.json() )
+      .then (msg => {
+        this.updateList(),
+        console.log(msg) 
+      })
+  }
+
+  toggleTask(id){
+    fetch("/task/" + id + "/toggle",{method:"put"})
+      .then( (resp) => resp.json() )
+      .then (msg => {
+        this.updateList(),
+        console.log(msg) 
+      })
   }
 
   render() {
+    const myList = this.state.tasks.map( task => 
+      <tr key={task.id}>
+        <td onClick={()=>this.toggleTask(task.id)} className={ task.done ? 'done' : '' } >{task.id + " " + task.title}</td>
+        <td><MyButton onClick={()=>this.deleteTask(task.id)} text = "Eliminar"></MyButton></td>
+      </tr>
+    )
     return (
-      <MyButton onToggle={this.handleClick} 
-		toggle={this.state.isToggleOn}/>
-    );
+      <div>
+        <h1>{this.state.pageTitle}</h1> 
+        <MyInput type="text" id="newTaskName" name="newTaskName"></MyInput>
+        <MyButton onClick={this.createTask} text = {this.state.buttonName}></MyButton>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarea</th>
+              <th>Acción</th>
+            </tr>
+          </thead>
+          <tbody>
+              {myList}
+          </tbody>
+        </table>      
+      </div>
+    )
   }
 }
 
+render(<App/>, document.getElementById('app'));
 
-render(<Toggle name="mundo!!"/>, document.getElementById('app'));
+//<ul>{myList}</ul>
